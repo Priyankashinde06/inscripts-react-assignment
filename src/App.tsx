9746b44a88a0b098bce9d0ae34fe8b259efb4829
Priyankashinde06 <ps662001@gmail.com>
2025-07-13 11:37:57 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
